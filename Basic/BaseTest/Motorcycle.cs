8a486247c45d10d03bd2fd795f06f2d5870e92b2
marquez <k00240j@gmail.com>
2020-03-28 14:32:30 +0800

    [新增]  Motocycle 與 TestMotocycle 類別

---
+++ Basic/BaseTest/Motorcycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTest
+{
+    abstract class Motorcycle
+    {
+        // Anyone can call this.
+        public void StartEngine() 
+        {
+
+        }
+        // Only derived classes can call this.
+        protected void AddGas(int gallons) 
+        { 
+
+        }
+        // Derived classes can override the base class implementation.
+        public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
+
+        // Derived classes can override the base class implementation.
+        public virtual int Drive(TimeSpan time, int speed) { /* Method statements here */ return 0; }
+
+        // Derived classes must implement this.
+        public abstract double GetTopSpeed();
+    }
+}
---
+++ Basic/BaseTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMotorcycle moto = new TestMotorcycle();
+
+            moto.StartEngine();
+            //moto.AddGas(15);
+            var drive = moto.Drive(5, 20);
+            double speed = moto.GetTopSpeed();
+            Console.WriteLine("My top speed is {0}", speed);
+            Console.ReadKey();
+        }
+    }
+}
