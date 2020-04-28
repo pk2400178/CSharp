f8d5736455657504a39f885dc1ba0e17fef8b40e
marquez <k00240j@gmail.com>
2020-04-29 00:18:57 +0800

    add Task

--- Basic/ThreadTest/ThreadPoolDemo.cs
+++ Basic/ThreadTest/ThreadPoolDemo.cs
@@ -19,9 +19,14 @@ namespace ThreadTest
             {
                 Console.WriteLine("工作取消 callback #1.");
             });
-            ThreadPool.QueueUserWorkItem(status => MyTask(cts.Token));
-            ThreadPool.QueueUserWorkItem(status => MyTask(cts.Token));
+            ThreadPool.QueueUserWorkItem(DownloadFile, "123");
+            ThreadPool.QueueUserWorkItem(status => MyTask(cts.Token,"456"));
+            ThreadPool.QueueUserWorkItem(status => MyTask(cts.Token,"789"));
             //ThreadPool.QueueUserWorkItem(state => MyTask(CancellationToken.None));
+
+            Task.Factory.StartNew(() => MyTask2(100,cts.Token));
+
+            Task.Run(() => MyTask2(100, cts.Token));
 
             Console.WriteLine("按 Enter 鍵可取消背景工作...");
             Console.ReadLine(); // 這會使 cts 的 IsCancellationRequested 屬性變成 true。
@@ -29,7 +34,7 @@ namespace ThreadTest
 
             Console.ReadLine();
         }
-        private void MyTask(CancellationToken cancellationToken)
+        private void MyTask(CancellationToken cancellationToken, string t)
         {
             for (int i = 0; i < 1000; i++)
             {
@@ -38,7 +43,7 @@ namespace ThreadTest
                     Console.WriteLine("使用者要求取消工作!");
                     return;
                 }
-                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}:{i}");
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}:{i}, t: {t}");
                 Thread.Sleep(200);
             }
             Console.WriteLine("MyTask 工作執行完畢。");
@@ -47,6 +52,21 @@ namespace ThreadTest
         private void DownloadFile(object fileName)
         {
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} : Downloading file {fileName}");
+        }
+
+        private void MyTask2(int i, CancellationToken cancellationToken)
+        {
+            for (int a = 0; a < i; a++)
+            {
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("MyTask2使用者要求取消工作!");
+                    return;
+                }
+                Thread.Sleep(200);
+                Console.WriteLine("工作執行緒 #{0}", Thread.CurrentThread.ManagedThreadId);
+            }
         }
     }
 }
